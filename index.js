4d1c9358e7b6e1e034a606e903b771316b308f7c
Ahmer-Siddiqui <siddiquiahmer970@gmail.com>
2023-06-19 21:38:08 +0500

    Make Search API

--- index.js
+++ index.js
@@ -5,32 +5,19 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/create',async (req, res)=>{
-    console.log(req.body)
-    let data = new Products(req.body)
-    let result = await  data.save();
-    console.log(data);
-    res.send("Done")
-})
-
-app.get('/list',async (req, res)=>{
-    console.log(req.body)
-    let data =  await Products.find();
-    console.log(data);
+app.get('/search/:key',async (req, res)=>{
+    console.log(req.params.key);
+    let data = await Products.find(
+        {
+            "$or":[
+                {"name":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}}
+            ]
+        }
+        );
+    
     res.send(data)
 })
-app.delete('/delete/:_id',async (req, res)=>{
-    console.log(req.params.id)
-    let data =  await Products.deleteOne({id:req.params.id});
-    console.log(data);
-    res.send("Done")
-})
 
-app.put('/update/:_id',async (req, res)=>{
-    console.log(req.params)
-    let data =  await Products.updateOne(req.params,{$set:{brand:"Apple"}});
-    console.log(data);
-    res.send("Done")
-})
 
 app.listen(5000)
