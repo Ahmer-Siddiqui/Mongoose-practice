1ec8e6522b5620ef5d5be50850e08d4c75117023
Ahmer-Siddiqui <siddiquiahmer970@gmail.com>
2023-06-19 21:05:39 +0500

    Make GET,PUT,DELETE API

--- index.js
+++ index.js
@@ -13,4 +13,24 @@ app.post('/create',async (req, res)=>{
     res.send("Done")
 })
 
+app.get('/list',async (req, res)=>{
+    console.log(req.body)
+    let data =  await Products.find();
+    console.log(data);
+    res.send(data)
+})
+app.delete('/delete/:_id',async (req, res)=>{
+    console.log(req.params.id)
+    let data =  await Products.deleteOne({id:req.params.id});
+    console.log(data);
+    res.send("Done")
+})
+
+app.put('/update/:_id',async (req, res)=>{
+    console.log(req.params)
+    let data =  await Products.updateOne(req.params,{$set:{brand:"Apple"}});
+    console.log(data);
+    res.send("Done")
+})
+
 app.listen(5000)
